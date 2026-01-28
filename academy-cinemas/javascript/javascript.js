2a8fa05f4bae2d575dd98cfb74ea2407379ddb62
Mike Busch <michael.john.busch@gmail.com>
2026-01-28 11:00:51 -0800

    Bootstrap w/ JQuery
    Shrink, Scroll , navbar collapse and close when clicked

--- academy-cinemas/javascript/javascript.js
+++ academy-cinemas/javascript/javascript.js
@@ -11,9 +11,11 @@ popoverTriggerList.forEach(function (element) {
     });
 });
 
-// Initialize toast
+// Initialize toasts only if they exist
 const toastElList = document.querySelectorAll('.toast')
-const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl))
+if (toastElList.length > 0) {
+    toastElList.forEach(toastEl => new bootstrap.Toast(toastEl))
+}
 
 // FUNCTION TO DISPLAY TOAST WITH SELECTED OPTIONS
 function displaySelectedMovieOptions() {
@@ -33,3 +35,32 @@ function displaySelectedMovieOptions() {
 function buyTickets() {
     displaySelectedMovieOptions();
 }
+
+// JQUERY
+// SHRINKS HEADER SIZE WHEN THE DOCUMENT IS SCROLLED DOWN BY 80 pixels
+$(document).on("scroll", function (){
+    // WHEN WEBPAGE IS SCROLLED DOWN FROM THE TOP BY 50PX THIS IF STATEMENT WILL TRIGGER
+    if($(document).scrollTop() > 50) {
+        // ONCE THE 50PX REQUIREMENT HAS BEEN MET ADD THE NAV-SHRINK CLASS SELECTOR TO THE SAME HTML ELEMENT 
+        // THAT HAS THE NAV CLASS
+        $("nav").addClass("nav-shrink");
+        // ADJUST THE POSITION OF THE MOBILE DROP MENU TO ACCOMODATE THE NEW HEIGHT DECREASE
+        $("div.navbar-collapse").css("margin-top", "-6px");
+    } else {
+        // IF THE WEBPAGE HAS NOT BEEN SCROLLED DOWN OR IS BACK TO THE TOP THE NAV-SHRINK CLASS SELECTOR IS 
+        // REMOVED FROM THE HTML ELEMENT WITH THE NAV CLASS SELECTOR
+        $("nav").removeClass("nav-shrink");
+        // THE MARGIN FOR THE DROP DOWN MENU IS NOW RETURNED TO IT'S ORIGINAL AMOUNT
+        $("div.nav-bar-collapse").css("margin-top", "14px");
+    }
+});
+
+// CLOSE MOBILE MENU WHEN A NAVIGATION LINK IS CLOSED
+$(document).ready(function (){
+    // ON CLICK WHEN AN ELEMENT CONTAINS JUST THE NAV-LINK CLASS AND NOT THE DROPDOWN-TOGGLE AND THEN ALSO
+    // CLOSE WHEN AN ELEMENT WITH THE CLASS .DROPDOWN-ITEM (EACH MOVIE LINK) HAS BEEN CLICKED
+    $(".nav-bar-nav").on('click', '.nav-link:not(".dropdown-toggle"), .dropdown-item', function(){
+        // COLLAPSE THE NAVBAR WHEN A LINK OR DROPDOWN ITEM IS CLICKED
+        $(".navbar-collapse").collapse('hide');
+    });
+});
